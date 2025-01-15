c7e7102cb20dc9cf28cb8cae95abb3880603122a
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2025-01-15 11:42:57 +0500

    15.01.2005

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,29 @@
 const startSection = document.querySelector(".start");
 if (startSection) {
-  const btn = startSection.querySelector(".btn");
+  const startText = startSection.querySelector(".start__text");
+  const startSelect = startSection.querySelector(".start__select");
+  const options = startSelect.querySelectorAll(".start__select-option");
+  const btnText = startText.querySelector(".btn");
+  const btnSelect = startSelect.querySelector(".btn");
 
-  btn.addEventListener("click", () => {
+  options.forEach((option, index, arr) => {
+    option.addEventListener("click", () => {
+      arr.forEach((item) => {
+        item.classList.remove("active");
+      });
+
+      option.classList.add("active");
+    });
+  });
+
+  btnText.addEventListener("click", () => {
+    startText.style.display = "none";
+    startSelect.style.display = "flex";
+  });
+
+  btnSelect.addEventListener("click", () => {
+    startSelect.style.display = "none";
+    startText.style.display = "flex";
     startSection.style.display = "none";
   });
 }
