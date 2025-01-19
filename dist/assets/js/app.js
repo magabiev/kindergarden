c422d25a8230808961643bfb3344778b78860d93
umar.rakhmaev <umar.rakhmaev@ic-group.ru>
2025-01-19 18:07:19 +0300

    umar changes

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,4 +1,4 @@
-const isAdmin = sessionStorage.getItem("is-admin") || "false";
+const userType = sessionStorage.getItem("user-type") || "";
 
 const startSection = document.querySelector(".start");
 if (startSection) {
@@ -16,17 +16,32 @@ if (startSection) {
       option.classList.add("active");
 
       if (option.textContent == "Мәгариф идарәсе") {
-        sessionStorage.setItem("is-admin", true);
+        sessionStorage.setItem("user-type", "admin");
 
-        const menuItems = document.querySelectorAll(
+        window.location = "/stats/general";
+      } else if (option.textContent == "Ата-ана") {
+        sessionStorage.setItem("user-type", "parent");
+
+        window.location = "/achievements";
+      } else if (option.textContent == "Тәрбияче") {
+        sessionStorage.setItem("user-type", "teacher");
+        window.location = "/exercises";
+      } else {
+        sessionStorage.setItem("user-type", "");
+        resetHeader();
+
+        const menuItems = header.querySelectorAll(
           ".header__menu .header__menu-item"
         );
-        let lastChild = menuItems[menuItems.length - 1];
-        let a = lastChild.querySelector("a");
 
-        a.href = "/stats/general";
-      } else {
-        sessionStorage.setItem("is-admin", false);
+        menuItems.forEach((item, index) => {
+          item.style.display = "flex";
+
+          if (index == menuItems.length - 1) {
+            let a = item.querySelector("a");
+            a.href = "/stats";
+          }
+        });
       }
 
       setTimeout(() => {
@@ -45,24 +60,134 @@ if (startSection) {
 
 // Header
 const header = document.querySelector(".header");
+
+function resetHeader() {
+  const baqchaName = header.querySelector(".header__name");
+  const author = header.querySelector(".header__author");
+  const name = author.querySelector(".header__author-name");
+  const role = author.querySelector(".header__author-role");
+  const avatar = author.querySelector(".header__author-avatar img");
+  const pupil = header.querySelector(".header__pupil");
+  const headerRight = header.querySelector(".header__content-right");
+
+  name.innerHTML = "Шәмсетдинова <br/> Айгөл Таһир кызы";
+  role.innerHTML = "Балалар бакчасы тәрбиячесе";
+  avatar.src = "/assets/img/avatar.png";
+
+  baqchaName.style.display = "flex";
+  pupil.style.display = "none";
+
+  headerRight.style.transform = "translateY(0)";
+  headerRight.style.gap = "20px";
+}
+
 if (header) {
   const menuItems = header.querySelectorAll(".header__menu .header__menu-item");
+  const headerRight = header.querySelector(".header__content-right");
   const name = header.querySelector(".header__name");
   const author = header.querySelector(".header__author");
   const idara = header.querySelector(".header__idara");
+  const pupil = header.querySelector(".header__pupil");
 
-  if (isAdmin == "true" && idara) {
+  if (userType == "admin" && idara) {
     idara.style.display = "flex";
     name.style.display = "none";
     author.style.display = "none";
+
+    headerRight.style.transform = "translateY(-5px)";
+    headerRight.style.gap = "30px";
   }
 
-  menuItems.forEach((item) => {
-    const a = item.querySelector("a");
+  if (userType == "parent" && author) {
+    const baqchaName = header.querySelector(".header__name");
+    const name = author.querySelector(".header__author-name");
+    const role = author.querySelector(".header__author-role");
+    const avatar = author.querySelector(".header__author-avatar img");
 
-    const isActive = a.href == window.location.href;
+    name.innerHTML = "Хисматуллина Алия <br/> Илдар кызы";
+    role.innerHTML = "Әни";
+    avatar.src = "/assets/img/ata-ana.jpg";
+
+    baqchaName.style.display = "none";
+    pupil.style.display = "flex";
+
+    headerRight.style.transform = "translateY(-5px)";
+    headerRight.style.gap = "30px";
+  }
+
+  menuItems.forEach((item, index) => {
+    if (userType == "admin") {
+      if (index == menuItems.length - 1) {
+        let a = item.querySelector("a");
+        a.href = "/stats/general";
+      } else {
+        item.style.display = "none";
+      }
+    }
+
+    if (userType == "parent") {
+      if (index != menuItems.length - 2) {
+        item.style.display = "none";
+      }
+    }
+
+    const a = item.querySelector("a");
+    const linkUrl = new URL(a.href, window.location.origin);
+
+    let isActive = false;
+    if (linkUrl.pathname === "/" && window.location.pathname === "/") {
+      isActive = true;
+    } else if (
+      linkUrl.pathname == "/exercises" &&
+      window.location.pathname.startsWith("/exercises")
+    ) {
+      isActive = true;
+    } else if (
+      linkUrl.pathname == "/achievements" &&
+      window.location.pathname.startsWith("/achievements")
+    ) {
+      isActive = true;
+    } else if (
+      linkUrl.pathname.includes("/stats") &&
+      window.location.pathname.startsWith("/stats")
+    ) {
+      isActive = true;
+    } else {
+      isActive = false;
+    }
+
     isActive && item.classList.add("active");
   });
+}
+
+const profile = document.querySelector(".intro__profile");
+if (profile) {
+  const name = profile.querySelector(".intro__profile-top .text-h2");
+  const desc = profile.querySelector(".intro__profile-top .text-h3");
+  const avatar = profile.querySelector(".intro__profile-top img");
+  const ratingItems = profile.querySelectorAll(
+    ".intro__profile-rating .flex-row"
+  );
+
+  if (userType == "parent") {
+    name.innerHTML = "Хисматуллин Илнур <br/> Айдар улы";
+    desc.innerHTML =
+      "Саба 6нчы «Балачак» балалар бакчасы, <br/> Урта төркем укучысы, 5 яшь";
+    // name.style.display = "none";
+    // desc.style.display = "none";
+    avatar.src = "/assets/img/little-boy.jpg";
+    avatar.style.objectPosition = "center 45%";
+
+    ratingItems[0].style.display = "flex";
+    ratingItems[1].style.display = "flex";
+    ratingItems[2].style.display = "none";
+    ratingItems[3].style.display = "none";
+  } else {
+    ratingItems[0].style.display = "none";
+    ratingItems[1].style.display = "none";
+    ratingItems[2].style.display = "flex";
+    ratingItems[3].style.display = "flex";
+  }
 }
 
 // ** Accordions ** //
@@ -111,40 +236,40 @@ const colorArray = [
 const chartExercises = document.getElementById("chart-1");
 if (chartExercises) {
   let exerciseData = [
-    { id: 0, name: "Югалтулар", result: 3 },
+    { id: 0, name: "Туган җирем", result: 3 },
     {
       id: 1,
-      name: "Өстәмә рәт",
+      name: "Казан-Башкалам ",
       result: 1,
     },
     {
       id: 2,
-      name: "Нәрсә авыррак?",
+      name: "Татарстанның символикасы ",
       result: 2,
     },
     {
       id: 3,
-      name: "Парны тап",
+      name: "Метро станцияләре ",
       result: 1,
     },
     {
       id: 4,
-      name: "Күләгәне тап",
+      name: "Югары һәм түбән",
       result: 2,
     },
     {
       id: 5,
-      name: "Күңелле исәп",
+      name: "Өстәмә рәт",
       result: 1,
     },
     {
       id: 6,
-      name: "Бала һәм әни",
+      name: "Югалтулар",
       result: 4,
     },
     {
       id: 7,
-      name: "3 кадәр саныйбыз",
+      name: "Каты һәм йомшак",
       result: 1,
     },
   ];
@@ -159,6 +284,7 @@ if (chartExercises) {
   const config = {
     type: "doughnut",
     data: {
+      labels: exerciseData.map((item) => item.name),
       datasets: [
         {
           label: "Exercises",
@@ -167,6 +293,20 @@ if (chartExercises) {
           hoverOffset: 4,
         },
       ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          callbacks: {
+            label: function (tooltipItem) {
+              return `${tooltipItem.raw}`;
+            },
+          },
+        },
+      },
     },
   };
 
@@ -183,6 +323,7 @@ if (chartCategories1) {
   const config = {
     type: "doughnut",
     data: {
+      labels: exerciseData.map((item) => item.name),
       datasets: [
         {
           label: "Exercises",
@@ -191,6 +332,20 @@ if (chartCategories1) {
           hoverOffset: 4,
         },
       ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          callbacks: {
+            label: function (tooltipItem) {
+              return `${tooltipItem.raw}`;
+            },
+          },
+        },
+      },
     },
   };
 
@@ -207,6 +362,7 @@ if (chartCategories2) {
   const config = {
     type: "doughnut",
     data: {
+      labels: exerciseData.map((item) => item.name),
       datasets: [
         {
           label: "Exercises",
@@ -215,6 +371,20 @@ if (chartCategories2) {
           hoverOffset: 4,
         },
       ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          callbacks: {
+            label: function (tooltipItem) {
+              return `${tooltipItem.raw}`;
+            },
+          },
+        },
+      },
     },
   };
 
@@ -231,9 +401,9 @@ if (chartCategories3) {
         "САБА 1НЧЕ «ШАТЛЫК» БАЛАЛАР БАКЧАСЫ",
         "САБА 2НЧЕ «ӘЛЛҮКИ» БАЛАЛАР БАКЧАСЫ",
         "САБА 4НЧЕ «КЫҢГЫРАУ» БАЛАЛАР БАКЧАСЫ",
-        "КОРСАБАШ «КУЯШКАЙ» БАЛАЛАР БАКЧАСЫ",
+        "КОРСАБАШ «КОЯШКАЙ» БАЛАЛАР БАКЧАСЫ",
         "ИСКЕ МИЧӘН «ТАН» БАЛАЛАР БАКЧАСЫ",
-        "ТИМЕРШИК «КУЯШКАЙ» БАЛАЛАР БАКЧАСЫ",
+        "ТИМЕРШИК «КОЯШКАЙ» БАЛАЛАР БАКЧАСЫ",
         "САБА 5НЧЕ «БӘЛӘКӘЧ» БАЛАЛАР БАКЧАСЫ",
       ],
       datasets: [
@@ -284,9 +454,9 @@ if (chartCategories4) {
         "САБА 1НЧЕ «ШАТЛЫК» БАЛАЛАР БАКЧАСЫ",
         "САБА 2НЧЕ «ӘЛЛҮКИ» БАЛАЛАР БАКЧАСЫ",
         "САБА 4НЧЕ «КЫҢГЫРАУ» БАЛАЛАР БАКЧАСЫ",
-        "КОРСАБАШ «КУЯШКАЙ» БАЛАЛАР БАКЧАСЫ",
+        "КОРСАБАШ «КОЯШКАЙ» БАЛАЛАР БАКЧАСЫ",
         "ИСКЕ МИЧӘН «ТАН» БАЛАЛАР БАКЧАСЫ",
-        "ТИМЕРШИК «КУЯШКАЙ» БАЛАЛАР БАКЧАСЫ",
+        "ТИМЕРШИК «КОЯШКАЙ» БАЛАЛАР БАКЧАСЫ",
         "САБА 5НЧЕ «БӘЛӘКӘЧ» БАЛАЛАР БАКЧАСЫ",
       ],
       datasets: [
@@ -335,7 +505,7 @@ if (chartCategories4) {
       },
       plugins: {
         legend: {
-          position: "top",
+          display: false,
         },
         tooltip: {
           callbacks: {
@@ -352,3 +522,83 @@ if (chartCategories4) {
 
   new Chart(chartCategories4, config);
 }
+
+const rangeFilters = document.querySelector(".range__filters");
+if (rangeFilters) {
+  const items = rangeFilters.querySelectorAll(".range__filters-item");
+
+  items.forEach((item, index, arr) => {
+    item.addEventListener("click", () => {
+      arr.forEach((item) => {
+        item.classList.remove("active");
+      });
+      item.classList.add("active");
+    });
+  });
+}
+
+// modal
+const modal = {
+  el: document.querySelector(".modal"),
+  blocks: document.querySelectorAll(".modal__content"),
+  open: function (name, animation = true) {
+    const target = this.el.querySelector(`[data-root=${name}]`);
+
+    this.el.classList.add("active");
+    this.el.style.display = "flex";
+    target.style.display = "flex";
+
+    if (animation) {
+      setTimeout(() => {
+        target.style.opacity = 1;
+        target.style.transform = "scale(1)";
+      }, 50);
+    } else {
+      target.style.opacity = 1;
+      target.style.transform = "scale(1)";
+    }
+  },
+  close: function (name, parent = true) {
+    if (!name) {
+      this.blocks.forEach((block) => {
+        block.style.opacity = 0;
+        block.style.transform = "scale(0.85)";
+
+        setTimeout(() => {
+          block.style.display = "none";
+        }, 350);
+      });
+    } else {
+      const target = this.el.querySelector(`[data-root=${name}]`);
+      target.style.opacity = 0;
+      target.style.transform = "scale(0.85)";
+
+      if (!parent) {
+        target.style.display = "none";
+      } else {
+        setTimeout(() => {
+          target.style.display = "none";
+        }, 350);
+      }
+    }
+
+    if (parent) {
+      setTimeout(() => {
+        this.el.classList.remove("active");
+        this.el.style.display = "none";
+      }, 350);
+    }
+  },
+};
+
+const modalTriggers = document.querySelectorAll("[data-modal]");
+modalTriggers.forEach((trigger) => {
+  trigger.addEventListener("click", () => {
+    const name = trigger.dataset.modal;
+    if (name !== "close") {
+      modal.open(name);
+    } else {
+      modal.close(null, true);
+    }
+  });
+});
